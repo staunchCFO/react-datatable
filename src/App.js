91a5f73f38a446bcb09575f9c37ae9380e701ccc
Emeka Okezie <okezieemeka949@gmail.com>
2022-06-09 07:22:08 +0100

    Created a React DataTable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { Row, Col, Card, Form } from 'react-bootstrap';
+import Table from "react-bootstrap-table-next";
+import Pagination from "react-bootstrap-table2-paginator";
+import * as Paginator from "react-bootstrap-table2-paginator";
+import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
+import { entries } from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const columns = [
+    { dataField: "id", text: "ID", hidden: true },
+    { dataField: "name", text: "Name", sort: true},
+    { dataField: "position", text: "Position", sort: true},
+    { dataField: "office", text: "Office", sort: true },
+    { dataField: "age", text: "Age", sort: true },
+    { dataField: "startDate", text: "Start date", sort: true },
+    { dataField: "salary", text: "Salary", formatter: (cell) => <span>${cell}</span> },
+    { dataField: "action", text: "Actions", cell:() => <button onClick="" >Actions</button>, button: true}
+  ];
+  
+  const customTotal = (from, to, size) => (
+    <div>
+      Showing {from} to {to} of {size} entries
+    </div>
+  );
+  
+  const customSizePerPage = (props) => {
+    const { options, currentSizePerPage, onSizePerPageChange } = props;
+  
+    const onPageChange = (e) => {
+      const page = e.target.value;
+      onSizePerPageChange(page);
+    }
+  
+    return (
+      <Row>
+        <Col xs="auto">
+          <Form.Select value={currentSizePerPage} onChange={onPageChange} className="pe-5">
+            {options.map(o => (
+              <option key={o.page} value={o.page}>
+                {o.text}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+        <Col xs="auto" className="d-flex align-items-center">
+          entries per page
+        </Col>
+      </Row>
+    );
+  };
+  
+  return(
+    <div className='px-5 py-5'>
+      <ToolkitProvider
+        keyField="id"
+        search={true}
+        columns={columns}
+        data={entries}
+        wrapperClasses="table-responsive"
+      >
+        {({ baseProps, searchProps }) => (
+          <Paginator.PaginationProvider pagination={
+            Pagination({
+              custom: true,
+              showTotal: true,
+              alwaysShowAllBtns: true,
+              totalSize: entries.length,
+              paginationTotalRenderer: customTotal,
+              sizePerPageRenderer: customSizePerPage
+            })
+          }>
+            {({ paginationProps, paginationTableProps }) => (
+              <Card>
+                <div className="table-responsive py-2">
+                  <Card.Header>
+                    <Row>
+                      <Col xs={12} md={6} className="py-1">
+                        <Paginator.SizePerPageDropdownStandalone {...paginationProps} />
+                      </Col>
+                      <Col xs={12} md={6} className="d-flex justify-content-md-end py-1">
+                        <Search.SearchBar {...searchProps} />
+                      </Col>
+                    </Row>
+                  </Card.Header>
+    
+                  <Table 
+                    {...baseProps} 
+                    {...paginationTableProps} 
+                    bodyClasses="border-1" 
+                    rowClasses="border-bottom"
+                    classes="table-bordered dataTable-table table-striped"
+                  />
+    
+                  <Card.Footer>
+                    <Row>
+                      <Col xs={12} md={6} className="d-flex align-items-center py-1">
+                        <Paginator.PaginationTotalStandalone {...paginationProps} />
+                      </Col>
+                      <Col xs={12} md={6} className="d-flex justify-content-md-end align-items-center mb-0 py-1">
+                        <Paginator.PaginationListStandalone {...paginationProps} />
+                      </Col>
+                    </Row>
+                  </Card.Footer>
+                </div>
+              </Card>
+            )}
+          </Paginator.PaginationProvider>
+        )}
+      </ToolkitProvider>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,402 @@
+const entries = [
+    {
+        id: 1,
+        name: "Emeka Okezie",
+        position: "Developer",
+        office: "Office 1",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 2,
+        name: "Mike Spencer",
+        position: "IT",
+        office: "Office 2",
+        age: 27,
+        startDate: "2021/06/09",
+        salary: "1034"
+    },
+    {
+        id: 3,
+        name: "Judith Cane",
+        position: "Accountant",
+        office: "Office 1",
+        age: 20,
+        startDate: "2021/06/08",
+        salary: "10400"
+    },
+    {
+        id: 4,
+        name: "Elizabeth Cane",
+        position: "Developer",
+        office: "Office 15",
+        age: 34,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Adative John",
+        position: "Developer",
+        office: "Office 17",
+        age: 24,
+        startDate: "2021/07/08",
+        salary: "10300"
+    },
+    {
+        id: 6,
+        name: "Jumoke John",
+        position: "Developer",
+        office: "Office 23",
+        age: 14,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 7,
+        name: "Jane John",
+        position: "Hackerr",
+        office: "Office 20",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 8,
+        name: "Isaac Osei",
+        position: "Business Owner",
+        office: "Office 4",
+        age: 44,
+        startDate: "2022/06/08",
+        salary: "103400"
+    },
+    {
+        id: 9,
+        name: "Iqbal Ibish",
+        position: "Lead Developer",
+        office: "Office 7",
+        age: 54,
+        startDate: "2020/06/08",
+        salary: "13400"
+    },
+    {
+        id: 10,
+        name: "Jamie Scott",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 11,
+        name: "Omerue Judith",
+        position: "Finance",
+        office: "Office 1",
+        age: 34,
+        startDate: "2021/08/08",
+        salary: "103500"
+    },
+    {
+        id: 12,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+    {
+        id: 5,
+        name: "Emeka",
+        position: "Developer",
+        office: "Office 11",
+        age: 24,
+        startDate: "2021/06/08",
+        salary: "103400"
+    },
+]
+
+export {
+    entries
+}
